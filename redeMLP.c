40873b7a4f82da4cbe5e3140b592340faab6a704
leosv76 <leo_sv76@hotmail.com>
2016-10-11 15:58:56 -0300

    Matriz Arquivo

--- redeMLP.c
+++ redeMLP.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define LINHAS 3
+#define COLUNAS 3
+#define PATH "teste.txt"
 /*Rede Neural Artificial Perceptron Multicamadas (MLP)*/
 
-int main(void){
-	printf("HelloWorld!!\n");
+void printData(double dados[LINHAS][COLUNAS]){
+	int i, j;
+	for(i = 0; i < LINHAS; i++){
+		for(j = 0; j < COLUNAS; j++){
+			printf("%.5f ", dados[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void createData(double dados[LINHAS][COLUNAS]){
+	int i,j;
+	FILE *arq = fopen(PATH,"r");
+
+	for( i= 0; i < LINHAS;i++){
+		for(j = 0; j < COLUNAS;j++){
+			fscanf(arq, "%lf",&dados[i][j]);
+		}
+	}
+
+}
+
+int main(){
+
+	double valor, dados[LINHAS][COLUNAS];
+	createData(dados);
+	printData(dados);
+	
+	return(0);
 }
